d0762d7e476f1f50f52c4e7d8263c8dd9ec06fff
HanPark-Ethan <ethan.hanp89@gmail.com>
2022-02-02 14:56:02 -0500

    updated routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EthanComponent } from './ethan/ethan.component';
+import { JaehyunComponent } from './jaehyun/jaehyun.component';
+import { JeongComponent } from './jeong/jeong.component';
+import { JeremiahComponent } from './jeremiah/jeremiah.component';
+import { MikeComponent } from './mike/mike.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"jaehyun",
+  component:JaehyunComponent
+}, {
+  path:"mike",
+  component:MikeComponent
+}, {
+  path:"jeong",
+  component:JeongComponent
+}, {
+  path:"jeremiah",
+  component:JeremiahComponent
+}, {
+  path:"ethan",
+  component:EthanComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
